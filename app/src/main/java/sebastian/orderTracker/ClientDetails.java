98729634ec87f3ea45e2a1eef137137ad5b5cda8
riseba <riseba@gmail.com>
2016-04-04 15:22:03 -0300

    Merge branch 'master' of https://github.com/hanoii/tallerproyectos2
    Agregada carga de imagen de Cliente en detalle

--- app/src/main/java/sebastian/orderTracker/ClientDetails.java
+++ app/src/main/java/sebastian/orderTracker/ClientDetails.java
@@ -1,10 +1,17 @@
 package sebastian.orderTracker;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -51,7 +58,33 @@ public class ClientDetails extends AppCompatActivity implements OnMapReadyCallba
         telefonoMovil.append(client.getMobilePhoneNumber());
         telefonoFijo.append(client.getStaticPhoneNumber());
         mail.append(client.getMailAdress());
+
+        final CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
+        ImageLoader mImageLoader = NetworkManagerSingleton.getInstance(this).getImageLoader();
+        //ImageView mImageView = new ImageView(this);
+        final ImageView mImageView = new ImageView(this);
+        //mImageLoader.get(c.getImgSrc(), ImageLoader.getImageListener(mImageView,
+        //        R.drawable.def_image, R.drawable.err_image));
+
+        mImageLoader.get(c.getImgSrc(),new ImageLoader.ImageListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                    mImageView.setImageResource(R.drawable.err_image);
+            }
+
+            @Override
+            public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+                if (response.getBitmap() != null) {
+                    mImageView.setImageBitmap(response.getBitmap());
+                    toolbarLayout.setBackground(mImageView.getDrawable());
+                }
+            }
+        });
+
+        //toolbarLayout.setBackground(mImageView.getDrawable());
     }
+
+
 
     @Override
     public void onMapReady(GoogleMap map) {
@@ -61,5 +94,7 @@ public class ClientDetails extends AppCompatActivity implements OnMapReadyCallba
                 .position(new LatLng(c.getLat(), c.getLng()))
                 .title(c.getName()));
     }
+
+
 
 }
